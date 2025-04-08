5f0240b66e9fb65d026b94be56673cfc1184bba5
robertaval <roberta_valyte@hotmail.com>
2025-04-08 21:07:40 +0000

    conditional profile

--- src/js/app.js
+++ src/js/app.js
@@ -1,65 +1,61 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  const cover = variables.includeCover
+    ? `<div class="cover"><img src="${variables.background}" /></div>`
+    : "<div class='cover'></div>";
+
+  const name = variables.name || "Name";
+  const lastName = variables.lastName || "Last Name";
+  const role = variables.role || "Role";
+  const city = variables.city || "City";
+  const country = variables.country || "Country";
+
+  const socialLinks = [
+    { name: "twitter", icon: "fab fa-twitter" },
+    { name: "github", icon: "fab fa-github" },
+    { name: "linkedin", icon: "fab fa-linkedin" },
+    { name: "instagram", icon: "fab fa-instagram" }
+  ];
+
+  const renderedSocials = socialLinks
+    .map(link =>
+      variables[link.name]
+        ? `<li><a href="https://${link.name}.com/${
+            variables[link.name]
+          }"><i class="${link.icon}"></i></a></li>`
+        : ""
+    )
+    .join("");
+
+  const positionClass =
+    variables.socialMediaPosition === "position-left"
+      ? "position-left"
+      : "position-right";
+
+  document.querySelector("#widget_content").innerHTML = `
+    <div class="widget">
+      ${cover}
+      <img src="${variables.avatarURL}" class="photo" />
+      <h1>${name} ${lastName}</h1>
+      <h2>${role}</h2>
+      <h3>${city}, ${country}</h3>
+      <ul class="${positionClass}">
+        ${renderedSocials}
+      </ul>
+    </div>`;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
+    background:
+      "https://images.pexels.com/photos/2946979/pexels-photo-2946979.jpeg?auto=compress&cs=tinysrgb&w=600",
+    avatarURL:
+      "https://images.pexels.com/photos/4445619/pexels-photo-4445619.jpeg?auto=compress&cs=tinysrgb&w=600",
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
     github: null,
     linkedin: null,
@@ -70,22 +66,25 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); // render the card for the first time
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
+  render(window.variables);
+
+  document.querySelectorAll(".picker").forEach(elm => {
+    elm.addEventListener("change", e => {
+      const attribute = e.target.getAttribute("data-attribute");
+      const value = e.target.value;
+
+      const parsedValue =
+        value === "" || value === "null"
           ? null
-          : this.value == "true"
+          : value === "true"
           ? true
-          : this.value == "false"
+          : value === "false"
           ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+          : value;
+
+      const updatedVars = { [attribute]: parsedValue };
+      render(Object.assign(window.variables, updatedVars));
     });
   });
 };
